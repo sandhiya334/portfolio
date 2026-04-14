11149ed05ccb2fc59ad989d13042430db514bab5
Sandy <sandhiyakr334@gmail.com>
2026-04-14 22:00:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function openAbout() {
+    document.getElementById("aboutPopup").style.display = "block";
+}
+
+function closeAbout() {
+    document.getElementById("aboutPopup").style.display = "none";
+}
